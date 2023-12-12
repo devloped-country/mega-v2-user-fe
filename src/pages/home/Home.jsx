5269ceb1d72c72ae286671027fe71187ce9280f2
ubeom kim <kimub1204@gmail.com>
2023-12-12 20:19:10 +0900

    fix: 로그인 페이지 아이디 입력안되는 버그 픽스

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,55 +1,46 @@
-import { useNavigate } from "react-router-dom";
-import Header from "../../components/Home/Header";
-import Detail from "../../components/Home/Detail";
-import NoticePreview from "../../components/Home/NoticePreview";
-import CurriculumPreview from "../../components/Home/CurriculumPreview";
-import AddBanner from "../../components/common/AddBanner";
+import { useNavigate } from 'react-router-dom';
+import Header from '../../components/Home/Header';
+import Detail from '../../components/Home/Detail';
+import NoticePreview from '../../components/Home/NoticePreview';
+import CurriculumPreview from '../../components/Home/CurriculumPreview';
+import AddBanner from '../../components/common/AddBanner';
 
 function Home() {
   const navigate = useNavigate();
 
   const handleAttendance = () => {
     navigate('attendance');
-  }
+  };
 
   const handleNotice = () => {
     navigate('notice');
-  }
+  };
 
   const handleCurriculum = () => {
     navigate('curriculum');
-  }
-  return(
+  };
+  return (
     <div>
-    <Header
-      headerText='병가ㆍ공가ㆍ조퇴를 신청해야하나요?'
-      onButtonAction={handleAttendance}
-    />
-    <Detail
-      title='공지사항'
-      onButtonAction={handleNotice}
-    />
-    <NoticePreview/>
-
-    <AddBanner/>
-
-    <Detail
-      title='커리큘럼'
-      onButtonAction={handleCurriculum}
-    />
-    <CurriculumPreview
-      subject='리눅스 시스템 이해하기'
-      time='35h'
-      startDate='23.05.25'
-      endDate='23.06.01'
-    />
-    <CurriculumPreview
-      subject='2차 프로젝트'
-      time='140h'
-      startDate='23.11.21'
-      endDate='23.12.18'
-    />
-    
+      <Header
+        headerText='병가ㆍ공가ㆍ조퇴를 신청해야하나요?'
+        onButtonAction={handleAttendance}
+      />
+      <Detail title='공지사항' onButtonAction={handleNotice} />
+      <NoticePreview />
+      <AddBanner />
+      <Detail title='커리큘럼' onButtonAction={handleCurriculum} />
+      <CurriculumPreview
+        subject='리눅스 시스템 이해하기'
+        time='35h'
+        startDate='23.05.25'
+        endDate='23.06.01'
+      />
+      <CurriculumPreview
+        subject='2차 프로젝트'
+        time='140h'
+        startDate='23.11.21'
+        endDate='23.12.18'
+      />
     </div>
   );
 }
