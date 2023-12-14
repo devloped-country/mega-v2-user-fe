cbed1be11484cbccb853d7eda989792c849112ca
soommin0019 <smlee0109@naver.com>
2023-12-14 10:45:59 +0900

    fix: 컷밋 병합

--- src/pages/info/Info.jsx
+++ src/pages/info/Info.jsx
@@ -11,6 +11,38 @@ function Info() {
     navigator('edit');
   }
 
+<<<<<<< Updated upstream
+=======
+  const {
+    data : user,
+    isLoading
+  } = useFetch(
+    [],
+    [],
+    async () => await axios(`/api/curriculum/read/${id}`)
+  )
+
+  if(isLoading) {
+    return 
+  }
+
+  const mapedUser = user.data.data.map(
+    ({user_id,  course_id, institution_id, name, phone, email}) => {
+      return (
+        <InfoList 
+          key={user_id}
+          id={user_id}
+          courseId={course_id}
+          institutionId={institution_id}
+          name={name}
+          phone={phone}
+          email={email}
+        />
+      )
+    }
+  )
+
+>>>>>>> Stashed changes
   return (
     <div className={styles.backColor}>
       <UserInfoHeader
