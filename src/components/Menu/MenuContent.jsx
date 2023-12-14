634bbfdade2ff666578cb6dffeee8d0b05a9f183
soommin0019 <smlee0109@naver.com>
2023-12-14 13:22:30 +0900

    "feature: 오류 해결"

--- src/components/Menu/MenuContent.jsx
+++ src/components/Menu/MenuContent.jsx
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import MenuHeader from './MenuHeader';
 import MenuList from './MenuList';
-import { useNavigate } from 'react-router-dom';
 
 function MenuContent() {
   const navigate = useNavigate();
