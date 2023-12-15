862e5acdd1629c9b445140146c0969294193dc55
ubeom kim <kimub1204@gmail.com>
2023-12-15 21:04:30 +0900

    fix: vite-config 수정

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3002,
+    port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:8082/api",
+      '/api': {
+        target: 'https://user.mzc-appmega.click/api',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
   resolve: {
     alias: [
-      { find: "@components", replacement: "/src/components" },
-      { find: "@", replacement: "/src" },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@', replacement: '/src' },
     ],
   },
 });
